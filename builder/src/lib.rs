658d50f8f302e9d1fb68efc2cea2c68c1365ced1
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-01-30 22:25:10 +0530

    Getting started with Derive Builder
    Passes first couple of tests

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,27 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast: DeriveInput = parse(input).unwrap();
 
-    unimplemented!()
+    let name = &ast.ident;
+    let bident = Ident::new(&format!("{}Builder", name), name.span());
+
+    let tokens = quote! {
+
+        pub struct #bident {
+        }
+
+        impl Command {
+            pub fn builder() -> #bident {
+                #bident {
+                }
+            }
+        }
+
+    };
+
+    TokenStream::from(tokens)
 }
